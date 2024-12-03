0cc16ddc97c930ab0e92b5d66d8818a5f2641522
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-12-03 15:40:09 +0200

    reset to clean app template

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Appbar } from "react-native-paper";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { PaperProvider, MD3DarkTheme } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+  const theme = MD3DarkTheme;
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <PaperProvider theme={theme}>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={() => { }} />
+          <Appbar.Content title="Title" />
+          <Appbar.Action icon="calendar" onPress={() => { }} />
+          <Appbar.Action icon="magnify" onPress={() => { }} />
+        </Appbar.Header>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* <Button>TEXT</Button>
+      <Text>Edit app/index.tsx to edit this screen.</Text> */}
+    </View>
+  );
+}
