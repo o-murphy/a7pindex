738c4b6bb21135072c1daca921f98616684df71b
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-12-06 15:31:42 +0200

    updated preview

--- components/ItemDetails.tsx
+++ components/ItemDetails.tsx
@@ -1,8 +1,8 @@
 import { ProfileIndexType } from "@/hooks/FilterHook"
 import React, { useEffect, useState } from "react"
-import { Dialog, FAB, Portal, Surface, Text } from "react-native-paper"
+import { Button, Dialog, Divider, FAB, Portal, Surface, Text } from "react-native-paper"
 import { fetchDetails, downloadProfile } from "@/hooks/FetchProfile";
-
+import { Linking, Platform } from "react-native";
 
 
 type ItemDetailsProps = {
@@ -14,9 +14,9 @@ type ItemDetailsProps = {
 
 const DataRow = ({ label, value }) => {
     return (
-        <Surface elevation={0} style={{flexDirection: "row", padding: 8, justifyContent: "space-between"}}>
-            <Text style={{margin: 4}} >{label}</Text>
-            <Text style={{margin: 4}} >{value}</Text>
+        <Surface elevation={0} style={{flexDirection: "row", paddingVertical: 4, justifyContent: "space-between"}}>
+            <Text style={{marginHorizontal: 4, flex: 1}} >{label}</Text>
+            <Text style={{marginHorizontal: 4, flex: 2}} >{value}</Text>
         </Surface>
     )
 }
@@ -33,10 +33,23 @@ const ItemDetails: React.FC<ItemDetailsProps> = ({ item, visible, onDismiss }) =
     return (
         <Portal>
             <Dialog visible={visible} onDismiss={onDismiss} style={{ maxWidth: 350, minWidth: 300, maxHeight: "50%", alignSelf: "center" }}>
-                <Dialog.Title>{item?.profileName}</Dialog.Title>
-                <Dialog.Content>
-                    <DataRow label={"Caliber"} value={data?.caliber}/>
-                    <DataRow label={"Bullet"} value={data?.bulletName}/>
+                <Dialog.Title>{item?.meta?.productName || data?.profileName}</Dialog.Title>
+                <Dialog.Content style={{paddingBottom: 0}}>
+                    <DataRow label={"Caliber"} value={item?.meta?.caliber || data?.caliber}/>
+                    <Divider />
+                    <DataRow label={"Cartridge vendor"} value={item?.meta?.vendor || item?.cartridgeVendor}/>
+                    <Divider />
+                    <DataRow label={"Bullet Type"} value={item?.meta?.bulletType || item?.bullet}/>
+                    <Divider />
+                    <DataRow label={"Bullet weight"} value={`${item?.weight?.toFixed(1)} gr`}/>
+                    <Divider />
+                    <DataRow label={"Bullet diameter"} value={`${item?.diameter?.toFixed(3)} inch`}/>
+                    <Divider />
+                    <DataRow label={"Muzzle velocity"} value={`${item?.meta?.muzzle_velocity.toFixed(0)} m/s`}/>
+                    <Divider />
+                    <DataRow label={"Drag model"} value={item?.dragModelType}/>
+                    {/* <DataRow label={"Source"} value={item?.meta?.url} /> */}
+                    {item?.meta?.url && <Button icon={"open-in-new"} mode={"outlined"} onPress={() => Linking.openURL(item?.meta?.url)}>Vendor page</Button>}
                 </Dialog.Content>
                 <Surface style={{ flexDirection: "row", padding: 8, flex: 1 }} elevation={0}>
                     <FAB style={{ margin: 4, flex: 1 }} label={"Download"} icon={"download"} onPress={() => downloadProfile(item.path)} />
