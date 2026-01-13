4383c94879d821ca7c0571a3c84c01ec474cc205
o-murphy <thehelixpg@gmail.com>
2026-01-13 11:08:43 +0200

    fix: update editor url

--- hooks/FetchProfile.tsx
+++ hooks/FetchProfile.tsx
@@ -6,7 +6,7 @@ import { decode } from "a7p-js";
 
 // Path to your protobuf file
 const PUBLIC_PATH = __DEV__ ? "/" : "/a7pindex/";
-const EDITOR_PATH = "/archerbc2-web";
+const EDITOR_URL = "https://o-murphy.github.io/archerbc2-web";
 const LIB_URL = "https://portfolio.o-murphy.net/a7p-lib/"
 
 // Utility function to convert array buffer to base64
@@ -86,8 +86,7 @@ export const openInEditor = async (path: string | undefined) => {
         );
 
         if (Platform.OS === "web" && payload) {
-            const url = `${window.location.origin}${EDITOR_PATH}?payload=${payload}`;
-            // const url = `https://portfolio.o-murphy.net${EDITOR_PATH}?payload=${payload}`;
+            const url = `${EDITOR_URL}?payload=${payload}`;
             const link = document.createElement("a");
             link.href = url;
             link.target = "_blank";
